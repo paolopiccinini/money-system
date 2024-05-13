cbe40aa2b1006139d4536856d6b1bb378fa47302
Paolo Piccinini <paolo.piccinini@checkalt.com>
2024-05-13 13:37:25 +0200

    fix regex

--- src/main/java/com/example/moneysystem/constants/Constants.java
+++ src/main/java/com/example/moneysystem/constants/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
 
     private Constants(){};
 
-    public static final String VALIDATION_REGEX_PSD = "(\\d+p)(\\s([0-9]|1[0-9]|20)s)?(\\s([0-9]|1[0-2])d)?";
-    public static final String VALIDATION_REGEX_SD = "(([0-9]|1[0-9]|20)s)?(\\s([0-9]|1[0-2])d)?";
-    public static final String VALIDATION_REGEX_D = "(([0-9]|1[0-2])d)?";
+    public static final String VALIDATION_REGEX_PSD = "(\\d+p)(\\s([0-9]|1[0-9])s)?(\\s([0-9]|1[0-1])d)?";
+    public static final String VALIDATION_REGEX_SD = "(([0-9]|1[0-9])s)?(\\s([0-9]|1[0-1])d)?";
+    public static final String VALIDATION_REGEX_D = "(([0-9]|1[0-1])d)?";
     public static final String VALIDATION_REGEX = "^(" + VALIDATION_REGEX_PSD + "|" + VALIDATION_REGEX_SD + "|" + VALIDATION_REGEX_D + ")";
     public static final int PENCE_IN_POUND = 240;
     public static final int PENCE_IN_SCHILLING = 12;
